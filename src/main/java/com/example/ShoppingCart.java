1fbdcc90d4fab07e2ff2b63480bb6dfe00bc550b
Anton <antongrubisic@outlook.com>
2025-01-30 14:45:36 +0100

    Initial commit for ShoppingCart and CartItems

---
+++ src/main/java/com/example/ShoppingCart.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ShoppingCart {
+    private List<CartItems> items;
+    public ShoppingCart() {
+        items = new ArrayList<>();
+    }
+}
