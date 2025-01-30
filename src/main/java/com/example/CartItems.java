1dacd24c42d6161a9a6154f2fa2ceb026fcce8e5
Anton <antongrubisic@outlook.com>
2025-01-30 14:51:28 +0100

    Getter and setters for class cartitems

--- src/main/java/com/example/CartItems.java
+++ src/main/java/com/example/CartItems.java
@@ -1,4 +1,33 @@
 package com.example;
 
 public class CartItems {
+    private final String itemName;
+    private int quantity;
+    private double price;
+
+    public CartItems(String itemName, int quantity, double price) {
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
