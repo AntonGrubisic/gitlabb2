0c687228df52eac74146bad619c7e5333e254197
Anton <antongrubisic@outlook.com>
2025-02-02 10:48:25 +0100

    Setting up beforeeach before testing

--- src/test/java/com/example/payment/payments/PaymentProcessorTest.java
+++ src/test/java/com/example/payment/payments/PaymentProcessorTest.java
@@ -1,7 +1,24 @@
 package com.example.payment.payments;
 
+import com.example.payment.database.DatabaseConnection;
+import com.example.payment.email.EmailService;
+import org.junit.jupiter.api.BeforeEach;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mock;
 
 class PaymentProcessorTest {
+    private PaymentProcessor paymentProcessor;
+    private PaymentApi paymentApiMock;
+    private DatabaseConnection databaseConnectionMock;
+    private EmailService emailServiceMock;
+
+    @BeforeEach
+    public void setUp() {
+        paymentApiMock = mock(PaymentApi.class);
+        databaseConnectionMock = mock(DatabaseConnection.class);
+        emailServiceMock = mock(EmailService.class);
+
+        paymentProcessor = new PaymentProcessor(paymentApiMock, databaseConnectionMock, emailServiceMock);
+    }
 
 }
